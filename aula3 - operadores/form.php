7ff654f750d276ff16a939a81a9c7a36eb31aa86
Codepeia <112626131+Codepeia@users.noreply.github.com>
2024-05-02 17:24:28 -0300

    Add files via upload

---
+++ aula3 - operadores/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="recebeDados.php" method="post">
+        Nome: <input type="text" name="nome"><br><br>
+        Entre com o primeiro numero : <input type="text" name="numero1" ><br><br>
+        Entre com o segundo numero: <input type="text" name="numero2"><br><br>
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
---
+++ aula3 - operadores/incrementoDecremento.php
@@ -0,0 +1,25 @@
+<?php
+//operador incremento (++)
+
+    $valor = 10;
+    $valor++;//soma 1
+    $valor += 30;//soma 30 ao valor anterior
+    $valor = $valor + 30;
+
+    echo "".$valor;
+
+    //operador de decremento
+
+    $valor--;
+    echo "\n".$valor;
+    $valor -= 20;
+    $valor = $valor - 20;
+    echo "\n".$valor;
+
+
+
+
+
+
+
+?>
